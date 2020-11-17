20dae973368e3d8a0488173a8f403357b3c0f20b
Niko Kalliomaa <nikall@utu.fi>
2020-11-17 19:00:08 +0200

    Fixed some issus with vagrant

--- frontend/src/components/Leftbar.js
+++ frontend/src/components/Leftbar.js
@@ -3,7 +3,6 @@ import React from "react";
 const Leftbar = () => {
     return (
         <div>
-            <h1>Verkkomedia</h1>
         </div>
     )
 }
--- frontend/src/components/Rightbar.js
+++ frontend/src/components/Rightbar.js
@@ -2,7 +2,6 @@ import React from "react";
 const Rightbar = () => {
     return (
         <div className="rightbar">
-            <h1>Verkkomedia</h1>
         </div>
     )
 }
